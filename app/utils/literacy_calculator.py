b2506f4a3e8964a59d7914a9db2db3c493ddd47f
digitalboy <digitalboyzone@gmail.com>
2025-03-04 15:18:16 +0800

    modified:   app/data/stories.json 	modified:   app/utils/literacy_calculator.py

--- app/utils/literacy_calculator.py
+++ app/utils/literacy_calculator.py
@@ -19,7 +19,7 @@ class LiteracyCalculator:
             string.punctuation
             + "！？｡。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃《》「」『』【】〔〕〖〗〘㙀〙〚〛〜〝〞〟〰–—‘'‛“”„‟…⋯᠁"
         )
-        # 建立中英文词性映射关系
+        # 添加词性映射
         self.pos_mapping = {
             "N": "名词",
             "V": "动词",
@@ -35,7 +35,7 @@ class LiteracyCalculator:
             "PN": "专有名词",
             "IDIOM": "成语",
             "PREP": "介词",
-            "UNKNOWN": "UNKNOWN",  #  保持 UNKNOWN 不变
+            "UNKNOWN": "UNKNOWN",  # 保持 UNKNOWN 不变
         }
 
     def _load_known_words(self, target_level: int) -> Set[Tuple[str, str]]:
@@ -69,25 +69,25 @@ class LiteracyCalculator:
 
     def calculate_vocabulary_rate(
         self, text: str, target_level: int
-    ) -> Tuple[int, float, List[Dict[str, Union[str, int, None]]]]:  # 修改返回类型
+    ) -> Tuple[int, float, List[Dict[str, Union[str, int, None]]]]:
         """
         计算文本的词数、生词率，并返回生词列表。
         """
-        # 使用正则表达式分词， 匹配中文词语和英文单词， 并提取词性和词语
+        # 使用正则表达式分词，匹配中文词语和英文单词，并提取词性和词语
         tokens = re.findall(r"([\w]+)\(([A-Z]+)\)|([^\w\s])", text, re.UNICODE)
         word_count = 0
-        unknown_words: List[Dict[str, Union[str, int, None]]] = []  # 修改类型
+        unknown_words: List[Dict[str, Union[str, int, None]]] = []
         known_words = self._load_known_words(target_level)
         unknown_word_count = 0
 
         for token in tokens:
             # token 结构为 (word, pos, symbol), 每次只match 一种，另外两种为 ""
             word, pos, symbol = token
-            # 如果是标点符号， 直接跳过
+            # 如果是标点符号，直接跳过
             if symbol and symbol in self.punctuation:
                 continue
 
-            # 如果是词语， 则进行后续处理
+            # 如果是词语，则进行后续处理
             if word and pos:
                 word = word.strip().lower()
                 pos = pos.strip().upper()
@@ -96,6 +96,8 @@ class LiteracyCalculator:
                     self.logger.warning(f"无效的词语或词性: {token}")
                     continue
                 word_count += 1
+                # 将英文词性映射为中文词性
+                mapped_pos = self.pos_mapping.get(pos, "UNKNOWN")
                 if (word, pos) not in known_words:
                     # 获取词汇信息
                     word_model = next(
@@ -112,23 +114,30 @@ class LiteracyCalculator:
                     else:
                         chaotong_level = None
 
-                    # 只添加大于等于 target_level 的词汇， 或者 words.json 中不存在的词汇
+                    # 只添加大于等于 target_level 的词汇，或者 words.json 中不存在的词汇
                     if (
                         chaotong_level is not None and chaotong_level >= target_level
                     ) or chaotong_level is None:
                         # 去重机制
                         if not any(
-                            d["word"] == word and d["pos"] == pos for d in unknown_words
+                            d["word"] == word and d["pos"] == mapped_pos
+                            for d in unknown_words
                         ):
                             unknown_words.append(
-                                {"word": word, "pos": pos, "level": chaotong_level}
-                            )  # 修改生词列表格式
+                                {
+                                    "word": word,
+                                    "pos": mapped_pos,
+                                    "level": chaotong_level,
+                                }
+                            )
                             unknown_word_count += 1
                 else:
-                    self.logger.debug(f"已知词：{word}, 词性: {pos}")
+                    self.logger.debug(f"已知词：{word}, 词性: {mapped_pos}")
 
         new_word_rate = unknown_word_count / word_count if word_count else 0.0
         self.logger.debug(
             f"text: {text}, target_level: {target_level}, word_count: {word_count}, new_word_rate: {new_word_rate}, unknown_words: {unknown_words}"
         )
         return word_count, new_word_rate, unknown_words
+
+
